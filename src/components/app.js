0ba8b90c7dbcc1401dd7c60e00ffbdae91ccd562
ckim618 <ckim618@gmail.com>
2017-11-08 17:50:07 -0800

    completed add item functionality

--- src/components/app.js
+++ src/components/app.js
@@ -3,12 +3,14 @@ import 'materialize-css/dist/css/materialize.min.css';
 import { Link, Route } from 'react-router-dom';
 import TodoList from './todo_list';
 import AddItem from './add_item'; 
+import SingleItem from './single_item';
 
 
 const App = () => (
     <div className="container">
         <Route exact path="/" component={TodoList}/>
         <Route path="/add-item" component={AddItem}/>
+        <Route path="/item/:id" component={SingleItem}/>
     </div>
 );
 
